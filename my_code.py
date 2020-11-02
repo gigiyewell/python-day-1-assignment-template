ba9c68453055a297c1f208ad89450dafe0de699a
gigiyewell <24gyewell@athenian.org>
2020-11-02 10:19:12 -0800

    data types

--- my_code.py
+++ my_code.py
@@ -1,3 +1,38 @@
 # This is an example of a comment that Python ignores. Use these to make notes as you write your code. You will also fill out the top comment with any help or resources you use.
 # Collaborators:               (fill this is on every assignment, even if the answer is "none")
 
+print('Hi B period')
+
+name = 'B period'
+print(name)
+print(f'Good morning {name}!!')
+
+x = 5
+print(x)
+x = 7
+print(x)
+y = 3.14159
+condition = True
+z = '5'
+
+
+print(type(z))
+
+
+print('What is your favorite food?')
+food = input()
+print(f'I like {food} too!')
+
+input('What is your favorite color? ')
+
+
+# data types - bool, str, int, float
+
+pets = input('How many pets do you have? ')
+print(pets)
+print(type(pets))
+
+
+pets_now = pets + 2
+print(pets_now)
+
